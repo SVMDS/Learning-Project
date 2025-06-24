9e99b3f5099b35241d8543eaf093efc71838cdab
callumdaw <callumcdaw@gmail.com>
2025-06-24 23:06:26 +0100

    Add flights data file as well as CTF style script template

---
+++ Script/aviationstacktest.py
@@ -0,0 +1,54 @@
+import requests
+import json
+
+# UNCOMMENT TO GET FRESH DATA. SAVING TO FILE TO AVOID REPEATED REQUESTS THAT USE API QUOTA
+# AVIATIONSTACK_URL = "http://api.aviationstack.com/v1/"
+# aviationstackapikey = '' # Replace with your actual API key
+# response = requests.get(f"{AVIATIONSTACK_URL}flights?access_key={aviationstackapikey}&flight_status=active").json()
+# with open("aviationstack_flights.json", "w") as file:
+#     json.dump(response, file, indent=4)
+
+# LOAD DATA FROM SAVED FILE TO PRACTICE QUERIES
+with open("aviationstack_flights.json", "r") as file:
+    activeflights = json.load(file)
+
+# TAKE THE LIST OF ACTIVE FLIGHTS AND PICK ONE AT RANDOM
+
+# FOR THAT FLIGHT, GET THE DETAILS OF THE AIRPORTs USING THE IATA CODE AND THE AVIATIONSTACK AIRPORTS API 
+# (Seems undocumented but can pass iata_code as a parameter in the URL like below)
+# https://api.aviationstack.com/v1/airports?access_key=...&iata_code=AKL
+
+# USING THE INFORMATION ABOUT THE AIRPORTS, USE THE COUNTRIES API TO GET THE POPULATION, CURRENCY, AND CAPITAL OF THE DEPATURE AND ARRIVAL COUNTRIES
+
+# DISPLAY THE FOLLOWING INFORMATION:
+# THESE FIELDS WILL COME FROM MARRYING THE FLIGHT DATA WITH THE AIRPORT DATA AND COUNTRY DATA WHICH IS USEFUL TO PRACTICE
+    # depature country
+    # depature country population
+    # depature country capital
+    # depature country region
+    # depature country languages
+    # depature country currency
+    # departure airport name
+    # arrival country
+    # arrival country population
+    # arrival country capital
+    # arrival country region
+    # arrival country languages
+    # arrival country currency
+    # arrival airport name
+# THIS WILL HELP DEAL WITH SOME DATETIME FORMATS, FIGURING OUT DIFFERENCE BETWEEN TIMES IS A FAIRLY COMMON TASK
+    # total flight estimated duration (ignoring delays)
+# A LITTLE BIT OF MATH
+    # time difference between departure and arrival locations
+# A LOT OF MATH, TO DO THIS IT SEEMS LIKE THERE ARE SOME EQUATIONS NEEDED WHICH CAN BE FOUND ONLINE
+    # distance between departure and arrival airports measured in kilometers and miles (STRETCH GOAL)
+# KNOWING THE CURRENCY EXCHANGE RATE IS NOT A DATASOURCE WE CURRENTLY HAVE, WILL NEED TO SOURCE THIS FROM ANOTHER API AND CALCULATE
+    # exchange rate from departure country to arrival country currency (STRETCH GOAL)
+
+# ONCE DATA IS DISPLAYED AND FLOWING WELL, REFACTOR THE CODE TO USE FUNCTIONS TO MAKE IT MORE READABLE AND MAINTAINABLE
+
+# THINK ABOUT ANYTHING THAT IS REPEATED AND COULD BE MADE INTO A FUNCTION, OR LOGIC WHICH COULD BE WRAPPED IN A FUNCTION
+
+# THINK ABOUT HOW TO HANDLE ERRORS AND EXCEPTIONS, SUCH AS IF A COUNTRY OR AIRPORT IS NOT FOUND
+
+# UPDATE THE SCRIPT TO USE THE AVIATIONSTACK FLIGHTS API TO GET AN ACTIVE FLIGHT AND DISPLAY THE INFORMATION, RATHER THAN USING THE SAVED FILE
--- Script/test.py
+++ Script/test.py
@@ -1,13 +1,19 @@
 import requests
-import json
 
 COUNTRIES_URL = "https://restcountries.com/v3.1/"
 country_request = input("Which country would you like to choose?: ")
 
-rawdata = requests.get(COUNTRIES_URL + "name/" + country_request).json()              
-print(rawdata[0]['name']['common'] + rawdata[0]['cca2'])
-print(rawdata[0]['currencies'])
-# print(rawdata[0]['currencies']['AUD']['symbol'])
+rawdata = requests.get(COUNTRIES_URL + "name/" + country_request)
+rawdatajson = requests.get(COUNTRIES_URL + "name/" + country_request).json()   
+print("Country found: " + rawdata[0]['name']['common'])
+print("Getting other countries in the region...")
 
-for cur in rawdata[0]['currencies']:
-    print(cur + ": " + rawdata[0]['currencies'][cur]['name'])
+
+byregion = requests.get(COUNTRIES_URL + "region/" + rawdata[0]['region'])
+for country in byregion:
+    countrydata = requests.get(COUNTRIES_URL + "name/" + country['name']['common'])
+    print(countrydata[0]['name']['common'])
+    print("Capital: " + countrydata[0]['capital'][0])
+    print("Population: " + str(countrydata[0]['population']))
+    print("Area: " + str(countrydata[0]['area']) + " km²")
+    print("Languages: " + ", ".join(countrydata[0]['languages'].values()))
